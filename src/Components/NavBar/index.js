9233d199eb392c32d6dcff86c70cf34d754eada8
MohammedAshraf <85584@global.ul.com>
2021-05-06 10:22:20 +0530

    added fonts

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink,
-} from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
   
 const Navbar = () => {
   return (
@@ -19,15 +14,72 @@ const Navbar = () => {
             About
           </NavLink>
           <NavLink to='/blog' activeStyle>
-            Events
+            Blog
           </NavLink>
           <NavLink to='/skills' activeStyle>
-            Annual Report
+            Skills
           </NavLink>
          </NavMenu>
        </Nav>
     </>
   );
 };
+
+
+
+
+
+ const Nav = styled.nav`
+  background: #63D471;
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
+  /* Third Nav */
+  /* justify-content: flex-start; */
+`;
+  
+ const NavLink = styled(Link)`
+  color: #808080;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-family: roboto-regular;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    color: #000000;
+  }
+`;
+  
+ const Bars = styled(FaBars)`
+  display: none;
+  color: #808080;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+  
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+  /* Second Nav */
+  /* margin-right: 24px; */
+  /* Third Nav */
+  /* width: 100vw;
+  white-space: nowrap; */
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
   
 export default Navbar;
